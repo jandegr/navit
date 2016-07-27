3bf8939a145dd6ca0ef76f563cfa39bc72a16e16
jandegr <jandegr@users.noreply.github.com>
2016-07-27 22:13:05 +0200

    Update build_iOS.sh

--- ci/build_iOS.sh
+++ ci/build_iOS.sh
@@ -3,8 +3,8 @@
 echo "build iOS"
 brew install cmake
 ls -la
-mkdir ../navit-build
+mkdir navit-build
 ls -la
-cd ../navit-build
+cd navit-build
 ls -la
 cmake -G Xcode ../ -DCMAKE_TOOLCHAIN_FILE=Toolchain/xcode-iphone.cmake -DUSE_PLUGINS=0 -DBUILD_MAPTOOL=0 -DSAMPLE_MAP=0 -DXSLTS=iphone 
