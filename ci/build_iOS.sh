eca05671ba82c08c862969a65b9b7088880da9b6
jandegr <jandegr@users.noreply.github.com>
2016-07-28 20:12:10 +0200

    Update build_iOS.sh

--- ci/build_iOS.sh
+++ ci/build_iOS.sh
@@ -19,7 +19,7 @@ ls -la
 mkdir navit-build
 cd navit-build
 cmake -G Xcode ../ -DCMAKE_TOOLCHAIN_FILE=Toolchain/xcode-iphone.cmake -DUSE_PLUGINS=0 -DBUILD_MAPTOOL=0 -DSAMPLE_MAP=0 -DXSLTS=iphone 
-xcodebuild -configuration RelWithDebInfo 
-ls -la navit/RelWithDebInfo-iphoneos/navit.app
-ls -la navit/RelWithDebInfo-iphoneos/navit.app/share
-echo "END OF build_iOS.sh"
+#xcodebuild -configuration RelWithDebInfo 
+#ls -la navit/RelWithDebInfo-iphoneos/navit.app
+#ls -la navit/RelWithDebInfo-iphoneos/navit.app/share
+echo "END OF build_iOS.sh, configuration only !!!!!!!!!!!!"
