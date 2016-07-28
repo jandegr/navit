a8a839660086f23999cdfeead91c8e3257847c8e
jandegr <jandegr@users.noreply.github.com>
2016-07-28 11:59:07 +0200

    Update build_iOS.sh

--- ci/build_iOS.sh
+++ ci/build_iOS.sh
@@ -4,12 +4,13 @@ echo "build iOS"
 brew install cmake
 brew install gettext
 brew link --force gettext
-cd $HOME
-wget ftp://ftp.imagemagick.org/pub/ImageMagick/binaries/ImageMagick-x86_64-apple-darwin15.4.0.tar.gz
-tar xvzf ImageMagick-x86_64-apple-darwin15.4.0.tar.gz
-export MAGICK_HOME="$HOME/ImageMagick-7.0.1"
-export PATH="$MAGICK_HOME/bin:$PATH"
-export DYLD_LIBRARY_PATH="$MAGICK_HOME/lib/"
+#cd $HOME
+#wget ftp://ftp.imagemagick.org/pub/ImageMagick/binaries/ImageMagick-x86_64-apple-darwin15.4.0.tar.gz
+#tar xvzf ImageMagick-x86_64-apple-darwin15.4.0.tar.gz
+#export MAGICK_HOME="$HOME/ImageMagick-7.0.1"
+#export PATH="$MAGICK_HOME/bin:$PATH"
+#export DYLD_LIBRARY_PATH="$MAGICK_HOME/lib/"
+brew install librsvg
 cd $HOME/git
 ls -la
 mkdir navit-build
