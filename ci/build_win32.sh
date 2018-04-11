b97ffbf579c5b3b6cab7be6550d1a1eb8b7869a3
jandegr <jandegr@users.noreply.github.com>
2018-04-11 12:51:43 +0200

    Update build_win32.sh

--- ci/build_win32.sh
+++ ci/build_win32.sh
@@ -2,7 +2,7 @@ apt-get update && apt-get install -y mingw32 mingw32-binutils mingw32-runtime de
 
 mkdir win32
 pushd win32
-cmake -Dbinding/python:BOOL=FALSE -DSAMPLE_MAP=n -DXSLTS=windows -DCMAKE_TOOLCHAIN_FILE=../Toolchain/mingw32.cmake ../ 
+cmake -Dbinding/python:BOOL=FALSE -DSAMPLE_MAP=n -DBUILD_MAPTOOL=n -DXSLTS=windows -DCMAKE_TOOLCHAIN_FILE=../Toolchain/mingw32.cmake ../ 
 make
 make package
 popd
