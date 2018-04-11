9e71ec5e8fa426952798adbb92f40c2c70cad002
jandegr <jandegr@users.noreply.github.com>
2018-04-11 16:43:36 +0200

    Update build_android.sh

--- ci/build_android.sh
+++ ci/build_android.sh
@@ -49,17 +49,17 @@ cd $BUILD_PATH
 export PATH=$ANDROID_NDK_BIN:$ANDROID_SDK_TOOLS:$ANDROID_SDK_PLATFORM_TOOLS:$PATH
 sdkmanager --list
 ls -la /home/circleci/project
-svn_rev=` cd ~/navit/; git log -1|grep git-svn-id:|cut -c 65-68`
+svn_rev=` cd /home/circleci/project/navit/; git log -1|grep git-svn-id:|cut -c 65-68`
 if [[ "$svn_rev" == "" ]]; then svn_rev="6136"; fi # Workaround for git-only builds
-sed -i -e "s/ANDROID_VERSION_INT=\"0\"/ANDROID_VERSION_INT=\"${svn_rev}\"/g" ~/navit/navit/android/CMakeLists.txt
-cp ~/navit/navit/android/CMakeLists.txt $CIRCLE_ARTIFACTS/
+sed -i -e "s/ANDROID_VERSION_INT=\"0\"/ANDROID_VERSION_INT=\"${svn_rev}\"/g" android/CMakeLists.txt
+cp android/CMakeLists.txt $CIRCLE_ARTIFACTS/
 
 cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_FILE -DCACHE_SIZE='(20*1024*1024)' -DAVOID_FLOAT=1 -DSAMPLE_MAP=n -DBUILD_MAPTOOL=n -DANDROID_API_VERSION=23 -DANDROID_NDK_API_VERSION=19 $SOURCE_PATH
 make || exit 1
 #if [[ "${CIRCLE_BRANCH}" == "master" ]]; then
-  make apkg-release && mv navit/android/bin/Navit-release-unsigned.apk $CIRCLE_ARTIFACTS/navit-$CIRCLE_SHA1-release-unsigned.apk
+  make apkg-release && mv android/bin/Navit-release-unsigned.apk $CIRCLE_ARTIFACTS/navit-$CIRCLE_SHA1-release-unsigned.apk
 #else
-  make apkg && mv navit/android/bin/Navit-debug.apk $CIRCLE_ARTIFACTS/navit-$CIRCLE_SHA1-debug.apk
+  make apkg && mv android/bin/Navit-debug.apk $CIRCLE_ARTIFACTS/navit-$CIRCLE_SHA1-debug.apk
 #fi
 #mv navit/android/bin/Navit-debug-unaligned.apk $CIRCLE_ARTIFACTS/navit-$CIRCLE_SHA1-debug-unaligned.apk
 
