b468ac3aa46308b55019b9c79d663f806f11cca6
jandegr <jandegr@users.noreply.github.com>
2018-05-21 18:48:31 +0200

    Add files via upload

--- navit/android/src/org/navitproject/navit/NavitAddress.java
+++ navit/android/src/org/navitproject/navit/NavitAddress.java
@@ -2,30 +2,30 @@ package org.navitproject.navit;
 
 public class NavitAddress {
 
-        int mResultType;
-        int mId;
+    int mResultType;
+    int mId;
     float mLat;
-        float mLon;
-        String mAddr;
-        private String mAddrExtras;
+    float mLon;
+    String mAddr;
+    private String mAddrExtras;
 
-        NavitAddress(int type, int id, float latitude, float longitude,
+    NavitAddress(int type, int id, float latitude, float longitude,
                      String address, String addrExtras) {
-            mResultType = type;
-            this.mId = id;
-            mLat = latitude;
-            mLon = longitude;
-            mAddr = address;
-            mAddrExtras = addrExtras;
-        }
+        mResultType = type;
+        this.mId = id;
+        mLat = latitude;
+        mLon = longitude;
+        mAddr = address;
+        mAddrExtras = addrExtras;
+    }
 
-        public String toString() {
-            if (mResultType == 2) { // housenumber
-                return this.mAddr;
-            }
-            if (mResultType == 1) { // street
-                return this.mAddr;
-            }
-            return (this.mAddr + " " + this.mAddrExtras);
+    public String toString() {
+        if (mResultType == 2) { // housenumber
+            return this.mAddr;
         }
+        if (mResultType == 1) { // street
+            return this.mAddr;
+        }
+        return (this.mAddr + " " + this.mAddrExtras);
+    }
 }
