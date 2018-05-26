10667bc75e87c35e1bc828a9a80ae5e234f8c344
jandegr <jandegr@users.noreply.github.com>
2018-05-26 12:46:49 +0200

    Add files via upload

---
+++ navit/android/src/org/navitproject/navit/NavitCamera.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.navitproject.navit;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+
+
+
+class NavitCamera extends SurfaceView implements SurfaceHolder.Callback {
+
+    private Camera mCamera;
+    private final String TAG = getClass().getName();
+
+    NavitCamera(Context context) {
+        super(context);
+        SurfaceHolder holder;
+        holder = getHolder();
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        Log.e(TAG,"Creator");
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, acquire the camera and tell it where
+        // to draw.
+        try {
+            mCamera = Camera.open();
+            mCamera.setPreviewDisplay(holder);
+        } catch (IOException exception) {
+            mCamera.release();
+            mCamera = null;
+            Log.e(TAG,"IOException");
+        }
+        Log.d(TAG,"surfaceCreated");
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // Surface will be destroyed when we return, so stop the preview.
+        // Because the CameraDevice object is not a shared resource, it's very
+        // important to release it when the activity is paused.
+        mCamera.stopPreview();
+        mCamera = null;
+        Log.d(TAG,"surfaceDestroyed");
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // Now that the size is known, set up the camera parameters and begin
+        // the preview.
+        Log.d(TAG,"surfaceChanged " + w + "x" + h);
+        mCamera.stopPreview();
+        Camera.Parameters parameters = mCamera.getParameters();
+        parameters.setPreviewSize(w, h);
+        mCamera.setParameters(parameters);
+        mCamera.startPreview();
+    }
+}
+
