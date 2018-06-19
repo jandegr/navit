aa120a07af3a1764ac43c3451e5c688fb1269b0a
jandegr <jandegr@users.noreply.github.com>
2018-06-19 09:37:27 +0200

    Update build_win64.sh

--- ci/build_win64.sh
+++ ci/build_win64.sh
@@ -2,7 +2,7 @@ apt-get update && apt-get install -y mingw-w64 default-jdk nsis libsaxonb-java
 
 mkdir win64
 pushd win64
-cmake -Dbinding/python:BOOL=FALSE -DSAMPLE_MAP=n -DBUILD_MAPTOOL=n -DXSLTS=windows -DCMAKE_TOOLCHAIN_FILE=../Toolchain/mingw32.cmake ../ 
+cmake -Dbinding/python:BOOL=FALSE -DSAMPLE_MAP=n -DBUILD_MAPTOOL=n -DXSLTS=windows -DCMAKE_TOOLCHAIN_FILE=../Toolchain/ mingw-w64-x86_64.cmake  ../ 
 make
 make package
 popd
