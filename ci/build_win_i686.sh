8cd7e564bb63700437c835e596a627b89dcb151b
jandegr <jandegr@users.noreply.github.com>
2018-06-19 14:39:29 +0200

    Update build_win_i686.sh

--- ci/build_win_i686.sh
+++ ci/build_win_i686.sh
@@ -1,12 +1,12 @@
 apt-get update && apt-get install -y mingw-w64 default-jdk nsis libsaxonb-java
 
-mkdir win64
-pushd win64
+mkdir win32
+pushd win32
 cmake -Dbinding/python:BOOL=FALSE -DSAMPLE_MAP=n -DBUILD_MAPTOOL=n -DXSLTS=windows -DCMAKE_TOOLCHAIN_FILE=../Toolchain/mingw-w64-i686.cmake  ../ 
 make
 make package
 popd
 
 ls -la
-ls -la win64
+ls -la win32
 cp win32/*.exe $CIRCLE_ARTIFACTS/
