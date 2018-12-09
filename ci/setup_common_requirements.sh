4adebb4049c922711d8078eab0c01138e4aa64a6
jandegr <jandegr@users.noreply.github.com>
2018-12-09 14:31:52 +0100

    Update setup_common_requirements.sh

--- ci/setup_common_requirements.sh
+++ ci/setup_common_requirements.sh
@@ -1,1 +1,1 @@
-apt-get update && apt-get install -y wget unzip cmake build-essential gettext util-linux git ssh
+apt-get update && apt-get install -y wget unzip cmake build-essential gettext util-linux git ssh librsvg2-bin
