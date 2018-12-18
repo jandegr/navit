26f6a7cca8a442063a2b46823f64db68d65c6af0
jandegr <jandegr@users.noreply.github.com>
2018-12-18 18:33:46 +0100

    W64pgsql (#48)
    * Update build_win64.sh
    
    * Update build_win64.sh
    
    * Update build_win64.sh
    
    * Update build_win64.sh
    
    * Update build_win64.sh
    
    * Update build_win64.sh
    
    * Update build_win64.sh
    
    * Update build_win64.sh
    
    * Update build_win64.sh
    
    * libpq-fe
    
    * libpq for maptool postgresql

--- ci/build_win64.sh
+++ ci/build_win64.sh
@@ -1,8 +1,21 @@
 apt-get update && apt-get install -y mingw-w64 default-jre nsis libsaxonb-java
 
+pushd .
+
+wget ftp.postgresql.org/pub/latest/postgresql-11.1.tar.gz
+tar -z -xf postgresql-11.1.tar.gz
+cd postgresql-11.1
+./configure --host=x86_64-w64-mingw32 --without-zlib --prefix=/usr/x86_64-w64-mingw32
+make
+make install
+
+popd
+
+ls -la
 mkdir win64
 pushd win64
-cmake -Dbinding/python:BOOL=FALSE -DSAMPLE_MAP=n -DBUILD_MAPTOOL=y -DXSLTS=windows -DCMAKE_TOOLCHAIN_FILE=../Toolchain/mingw-w64-x86_64.cmake  ../ 
+ls -la
+cmake -Dbinding/python:BOOL=FALSE -DSAMPLE_MAP=n -DBUILD_MAPTOOL=y -DHAVE_POSTGRESQL=y -DXSLTS=windows -DCMAKE_TOOLCHAIN_FILE=../Toolchain/mingw-w64-x86_64.cmake  ../ 
 make
 make package
 popd
