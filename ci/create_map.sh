146f9965c749c2fc167a3027ad219b46cae34ff3
jandegr <jandegr@users.noreply.github.com>
2019-05-22 18:18:54 +0200

    Delete create_map.sh

--- ci/create_map.sh
+++
@@ -1,3 +0,0 @@
-- wget http://m.m.i24.cc/osmconvert.c && gcc osmconvert.c -lz -o osmconvert
-
-- wget -c -O  ~/europe-latest-complete.osm.pbf http://ftp.snt.utwente.nl/pub/misc/openstreetmap/europe-latest.osm.pbf
