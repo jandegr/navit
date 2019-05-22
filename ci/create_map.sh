3b5e2dc30631d4438d0dc4524607016ad730c5f1
jandegr <jandegr@users.noreply.github.com>
2019-05-22 18:18:21 +0200

    Create create_map.sh

---
+++ ci/create_map.sh
@@ -0,0 +1,3 @@
+- wget http://m.m.i24.cc/osmconvert.c && gcc osmconvert.c -lz -o osmconvert
+
+- wget -c -O  ~/europe-latest-complete.osm.pbf http://ftp.snt.utwente.nl/pub/misc/openstreetmap/europe-latest.osm.pbf
