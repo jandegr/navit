6006a1f9d9f5c6779b47dd6c8bcdd88a6195b978
jandegr <jandegr@users.noreply.github.com>
2019-06-05 11:49:16 +0200

    Update get_OSM_data.sh

--- scripts/get_OSM_data.sh
+++ scripts/get_OSM_data.sh
@@ -1,10 +1,15 @@
 set -e
 
+ls -la
+ls -la/linux
+
+echo ""
 echo "downloading PBF"
+echo""
 
 wget -c -O  europe-latest-complete.osm.pbf http://ftp.snt.utwente.nl/pub/misc/openstreetmap/europe-latest.osm.pbf
 # wget -c -O  europe-latest-complete.osm.pbf http://download.geofabrik.de/europe-latest.osm.pbf
 wget http://m.m.i24.cc/osmconvert.c && gcc osmconvert.c -lz -o osmconvert
 ls -la 
-./osmconvert --complete-ways --drop-author --verbose ~/europe-latest-complete.osm.pbf -B=BFR.poly -o=europe-latest.osm.pbf
+./osmconvert --complete-ways --drop-author --verbose europe-latest-complete.osm.pbf -B=BFR.poly -o=europe-latest.osm.pbf
 ls -la
