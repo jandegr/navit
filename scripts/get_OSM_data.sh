834bdb817adec44a59f6a07ff56563bd36979310
jandegr <jandegr@users.noreply.github.com>
2019-06-05 12:49:29 +0200

    Update get_OSM_data.sh

--- scripts/get_OSM_data.sh
+++ scripts/get_OSM_data.sh
@@ -14,5 +14,5 @@ wget -c -O  europe-latest-complete.osm.pbf http://ftp.snt.utwente.nl/pub/misc/op
 # wget -c -O  europe-latest-complete.osm.pbf http://download.geofabrik.de/europe-latest.osm.pbf
 wget http://m.m.i24.cc/osmconvert.c && gcc osmconvert.c -lz -o osmconvert
 ls -la 
-./osmconvert --complete-ways --drop-author --verbose europe-latest-complete.osm.pbf -B=BFR.poly -o=europe-latest.osm.pbf
-ls -la
+# ./osmconvert --complete-ways --drop-author --verbose europe-latest-complete.osm.pbf -B=BFR.poly -o=europe-latest.osm.pbf
+# ls -la
