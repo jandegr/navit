7ff9493f8d713d12d32616df0a42963b282feab0
jandegr <jandegr@users.noreply.github.com>
2019-06-05 11:01:21 +0200

    Update get_OSM_data.sh

--- scripts/get_OSM_data.sh
+++ scripts/get_OSM_data.sh
@@ -4,5 +4,5 @@ echo "downloading PBF"
 
 wget -c -O  ~/europe-latest-complete.osm.pbf http://ftp.snt.utwente.nl/pub/misc/openstreetmap/europe-latest.osm.pbf
 wget http://m.m.i24.cc/osmconvert.c && gcc osmconvert.c -lz -o osmconvert
-ls -la /home/ubuntu/navit
+ls -la 
 ./osmconvert --complete-ways --drop-author --verbose ~/europe-latest-complete.osm.pbf -B=/home/ubuntu/navit/BFR.poly -o=/home/ubuntu/europe-latest.osm.pbf
