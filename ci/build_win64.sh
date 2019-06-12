70cd48d9bc2547e215a10ca12d429aac40e2fbbf
jandegr <jandegr@users.noreply.github.com>
2019-06-12 16:02:47 +0200

    Update build_win64.sh

--- ci/build_win64.sh
+++ ci/build_win64.sh
@@ -1,6 +1,15 @@
 apt-get update && apt-get install -y mingw-w64 default-jre nsis libsaxonb-java
 
+# pushd .
 
+# wget ftp.postgresql.org/pub/latest/postgresql-11.3.tar.gz
+# tar -z -xf postgresql-11.3.tar.gz
+# cd postgresql-11.3
+# ./configure --host=x86_64-w64-mingw32 --without-zlib --prefix=/usr/x86_64-w64-mingw32
+# make
+# make install
+
+popd
 
 ls -la
 mkdir win64
