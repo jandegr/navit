c3983bda70c8aed4f9bde22c01955a1084d85158
jandegr <jandegr@users.noreply.github.com>
2019-09-10 18:23:37 +0200

    Update test_junk.c

--- navit/test_junk.c
+++ navit/test_junk.c
@@ -1,9 +1,8 @@
 
-void navit_foo_bar(struct point_rect * r, struct point * in, int count_in, struct point *out,
+void navit_foo_bar_before(struct point_rect * r, struct point * in, int count_in, struct point *out,
                                   int* count_out) {
     
-    const int limit=10000;
-    
+    const int limit=5689;
     struct point *temp=g_alloca(sizeof(struct point) * (count_in < limit ? count_in*8+1:0));
     struct point *pout;
     struct point *pin;
@@ -16,64 +15,38 @@ void navit_foo_bar(struct point_rect * r, struct point * in, int count_in, struc
     }
 
     
-    if (count_in >= limit) {
-        
+    if (count_in >= limit) {      
         temp=g_new(struct point, count_in*8+1);
     }
     
     pout = temp;
-    
     pin=in;
-    
     count=count_in;
 
-    /* clip all four directions of a rectangle */
     for (edge = 0 ; edge < 4 ; edge++) {
-        int i;
-        
-        struct point *p=pin;
-       
-        struct point *s=pin+count-1;
-        
-        *count_out=0;
-
-        /* iterate all points in current buffer */
         if (foo) {
             if (is_inside(p, r, edge)) {
                 if (! is_inside(s, r, edge)) {
-                    struct point pi;
-                    
-                    poly_intersection(s,p,r,edge,&pi);
-                    pout[(*count_out)++]=pi;
-                }
-               
-                pout[(*count_out)++]=*p;
+                    some_action();
+                }    
+             some_action2();  
             } else {
                 if (is_inside(s, r, edge)) {
-                    struct point pi;
-                    
-                    poly_intersection(p,s,r,edge,&pi);
-                    pout[(*count_out)++]=pi;
+                   some_action3()
                 }
-                
-            }
-            
-            s=p;
-            p++;
+            }           
+           do_something_here_too();
         }
         
         count=*count_out;
 
         if(pout == temp) {
-            pout=out;
-            pin=temp;
+            smth();
         } else {
-            pin=out;
-            pout=temp;
+            smth_else();
         }
     }
 
-    
     if (count_in >= limit) {
         g_free(temp);
     }
