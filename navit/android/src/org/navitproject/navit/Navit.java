690d2a4973dee4adf44373702582b4af665ab649
jandegr <jandegr@users.noreply.github.com>
2019-10-09 14:13:33 +0200

    fixes

--- navit/android/src/org/navitproject/navit/Navit.java
+++ navit/android/src/org/navitproject/navit/Navit.java
@@ -372,7 +372,7 @@ public class Navit extends Activity {
             }
         }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            if (Build.VERSION.SDK_INT >=Build.VERSION_CODES.LOLLIPOP) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 getWindow().getDecorView().setSystemUiVisibility(
                         View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
