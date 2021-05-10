d5f601d6f7c9aff711af2f5a900a5ab807397845
jandegr <jandegr@users.noreply.github.com>
2021-05-10 21:10:15 +0200

    Update build_win64_glib_dll.sh

--- ci/build_win64_glib_dll.sh
+++ ci/build_win64_glib_dll.sh
@@ -63,7 +63,7 @@ cd /tmp
 wget -c http://ftp.gnome.org/pub/gnome/sources/glib/2.9/glib-2.9.6.tar.gz
 tar xvzf glib-2.9.6.tar.gz
 cd glib-2.9.6
-mkdir build
+# mkdir build
 cd build
 meson --prefix=$PREFIX --cross-file=/tmp/cross_file_mingw64.txt --buildtype=plain ../
 
